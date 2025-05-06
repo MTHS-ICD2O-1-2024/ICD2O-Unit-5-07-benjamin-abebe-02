4e5b6f5fdf148a19e0325fa04df89ea1aca6d7c5
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-06 17:16:03 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// 
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// process
+function myButtonClicked() {
+  const userNumber = parseInt(document.getElementById("user-input").value)
+  let result = 0
+
+  for (let i = 1; i <= userNumber; i++) {
+    result += i
+  }
+
+  // display results
+  document.getElementById("answer").innerHTML = "<p>The sum from 1 to " + userNumber + " is: " + result + ".</p>"
+}
